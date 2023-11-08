311239566fb74ac2b2a40d1f9588f297f218f9e8
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-11-08 11:27:23 +0000

    part 4 done

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
+const { Sequelize } = require("sequelize");
 
-//TODO: Create your GET Request Route Below:
 app.get("/restaurants", async (req, res) => {
   const findRestaurants = await Restaurant.findAll();
   res.json(findRestaurants);
